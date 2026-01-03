82ca7f14c930b235454a88e4dbb5bce2d42d8913
Marius-Juston <mjuston2@illinois.edu>
2026-01-03 12:49:10 -0600

    disable timer function for Windows

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from glob import glob
 
 from setuptools import Extension
@@ -12,9 +13,19 @@ amd_config_dir = os.path.join("SuiteSparse", "SuiteSparse_config")
 amd_sources = glob(os.path.join(amd_source_dir, "*.c"))
 amd_config_sources = glob(os.path.join(amd_config_dir, "*.c"))
 
+define_macros = []
+
+if sys.platform == "win32":
+    define_macros += [
+        ("SUITESPARSE_HAVE_CLOCK_GETTIME", "0"),
+        ("SUITESPARSE_CONFIG_HAS_OPENMP", "0"),
+        ("SUITESPARSE_TIMER_ENABLED", "0"),
+    ]
+
 setup(version="0.1.0",
       ext_modules=[Extension(name="suitesparse_amd._amd",
                              sources=['src/suitesparse_amd/_amd.c'] + amd_sources + amd_config_sources,
                              include_dirs=[amd_include_dir, amd_config_dir],
                              language="c",
+                             define_macros=define_macros,
                              extra_compile_args=["-O3"], )])
