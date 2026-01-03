b3fc2d9ed23229e8436d77a68dd8015ea4a1458b
Marius-Juston <mjuston2@illinois.edu>
2026-01-03 00:00:34 -0600

    minor pyproject.toml update

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ amd_config_dir = os.path.join("SuiteSparse", "SuiteSparse_config")
 amd_sources = glob(os.path.join(amd_source_dir, "*.c"))
 amd_config_sources = glob(os.path.join(amd_config_dir, "*.c"))
 
-setup( ext_modules=[Extension(name="suitesparse_amd._amd",
+setup(version="0.1.0",
+      ext_modules=[Extension(name="suitesparse_amd._amd",
                              sources=['src/suitesparse_amd/_amd.c'] + amd_sources + amd_config_sources,
                              include_dirs=[amd_include_dir, amd_config_dir],
                              language="c",
