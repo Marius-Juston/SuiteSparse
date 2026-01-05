602b4382ff66a25d87653d2f55c6dd6ed933a503
Marius-Juston <mjuston2@illinois.edu>
2026-01-04 22:31:26 -0600

    implemented numpy array tests

--- tests/TestAMD.py
+++ tests/TestAMD.py
@@ -1,7 +1,22 @@
 import unittest
 from pathlib import Path
+from unittest import skipIf
 
 from suitesparse_amd.amd import amd
+
+try:
+    import torch
+
+    HAS_PYTORCH = True
+except ImportError:
+    HAS_PYTORCH = False
+
+try:
+    import numpy as np
+
+    HAS_NUMPY = True
+except ImportError:
+    HAS_NUMPY = False
 
 
 class TestList(unittest.TestCase):
@@ -124,6 +139,114 @@ class TestList(unittest.TestCase):
                 data = list(map(int, line.split(',')))
 
                 matrix[data[0]][data[1]] = data[2]
+
+        p, info = amd(matrix, dense_permutation=False)
+
+        self.assertEqual(p, expected_p)
+
+
+@skipIf(not HAS_NUMPY, "Numpy is not installed")
+class TestNumpy(unittest.TestCase):
+    def test_one_d_empty(self):
+        matrix = np.array([])
+
+        with self.assertRaises(ValueError):
+            p, v = amd(matrix)
+
+    def test_one_d_full(self):
+        matrix = np.array([0])
+
+        with self.assertRaises(ValueError):
+            p, v = amd(matrix)
+
+    def test_two_d_empty(self):
+        matrix = np.array([[]])
+
+        with self.assertRaises(ValueError):
+            p, v = amd(matrix)
+
+    def test_two_d_square_not_number(self):
+        matrix = np.array([[0, 0], [0, 's']])
+
+        with self.assertRaises(ValueError):
+            amd(matrix)
+
+    def test_two_d_square_identity_dense(self):
+        matrix = np.eye(10)
+
+        p, info = amd(matrix, dense_permutation=True)
+
+        self.assertTrue(np.allclose(matrix, p))
+
+    def test_two_d_square_identity(self):
+        matrix = np.array([[1, 0], [0, 1]])
+
+        p, info = amd(matrix, dense_permutation=False)
+
+        self.assertEqual(p, [0, 1])
+
+    def test_two_d_square_simple(self):
+        matrix = np.array([[1, 1, 1, 1],
+                           [1, 1, 0, 0],
+                           [1, 0, 1, 0],
+                           [1, 0, 0, 1]])
+
+        p, info = amd(matrix, dense_permutation=False)
+
+        self.assertEqual(p, [3, 2, 1, 0])
+
+    def test_two_d_square_simple_2(self):
+        matrix = np.array([[1, 0, 0, 1],
+                           [0, 1, 0, 0],
+                           [0, 0, 1, 0],
+                           [1, 0, 0, 1]])
+
+        p, info = amd(matrix, dense_permutation=False)
+
+        self.assertEqual(p, [1, 2, 0, 3])
+
+    def test_two_d_square_simple_3(self):
+        matrix = np.array([[1, 0, 0, 1],
+                           [0, 1, 1, 0],
+                           [0, 1, 1, 0],
+                           [1, 0, 0, 1]])
+
+        p, info = amd(matrix, dense_permutation=False)
+
+        self.assertEqual(p, [1, 2, 0, 3])
+
+    def test_two_d_square_simple_4(self):
+        matrix = np.array([[1, 1, 1, 0],
+                           [1, 1, 1, 0],
+                           [1, 1, 1, 1],
+                           [0, 0, 1, 1]])
+
+        p, info = amd(matrix, dense_permutation=False)
+
+        self.assertEqual(p, [3, 0, 1, 2])
+
+    def test_two_d_square_complex(self):
+        """
+        Checks if we get the expected output from the matlab example https://www.mathworks.com/help/matlab/ref/amd.html
+        :return:
+        """
+
+        base_dir = Path(__file__).parent
+
+        with open(base_dir / "P.csv") as f:
+            expected_p = list(map(int, f.readline().split(',')))
+
+        n = len(expected_p)
+
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+
+        with open(base_dir / "A.csv") as f:
+            for line in f.readlines():
+                data = list(map(int, line.split(',')))
+
+                matrix[data[0]][data[1]] = data[2]
+
+        matrix = np.array(matrix)
 
         p, info = amd(matrix, dense_permutation=False)
 
