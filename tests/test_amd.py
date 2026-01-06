43e417235ad6a58f0881e255dafeda4a19448e6a
Marius-Juston <mjuston2@illinois.edu>
2026-01-05 22:13:02 -0600

    ignore line too long for test_amd

--- tests/test_amd.py
+++ tests/test_amd.py
@@ -406,6 +406,7 @@ class TestNumpy(unittest.TestCase):
 
             p, _ = amd(matrix, dense_permutation=True)
 
+            #pylint: disable=line-too-long
             self.assertTrue(np.allclose(matrix, p), f"Failed to handle numpy array of type: {dtype}")
 
     def test_two_d_square_identity(self):
